fd148ff627849f87dee255b7d8ff5f9c6decb315
nicolasgodoy <nicolasgodoy540@gmail.com>
2025-02-07 19:08:05 -0300

    subiendooo

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -10,7 +10,7 @@ export class UsuarioService {
 
   constructor(private http:HttpClient) {}
   
-  url:string = "https://crud-angular-one.vercel.app.SQL1002.site4now.net/api/Usuario";
+  url:string = "https://SQL1002.site4now.net/api/Usuario";
   //url:string = "http://www.apiangularapp.somee.com/api/Usuario";
   // url:string = "https://localhost:44330/api/Usuario"; // PARA TRABAJAR LOCAL
   
