d235cc62ea8669b98393842ce7abd6609d668531
nicolasgodoy <nicolasgodoy540@gmail.com>
2025-02-07 20:57:23 -0300

    probando somee

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -10,8 +10,7 @@ export class UsuarioService {
 
   constructor(private http:HttpClient) {}
   
-  url:string = "https://SQL1002.site4now.net/api/Usuario";
-  //url:string = "http://www.apiangularapp.somee.com/api/Usuario";
+  url:string = "http://dominioangular.somee.com//api/Usuario";
   //url:string = "https://localhost:44330/api/Usuario"; // PARA TRABAJAR LOCAL
   
 
