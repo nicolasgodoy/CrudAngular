b08c569f60dc51c91084fe4d6482006e5e2e09ad
nicolasgodoy <nicolasgodoy540@gmail.com>
2025-02-07 19:47:19 -0300

    ultima prueba

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -12,7 +12,7 @@ export class UsuarioService {
   
   url:string = "https://SQL1002.site4now.net/api/Usuario";
   //url:string = "http://www.apiangularapp.somee.com/api/Usuario";
-  // url:string = "https://localhost:44330/api/Usuario"; // PARA TRABAJAR LOCAL
+  //url:string = "https://localhost:44330/api/Usuario"; // PARA TRABAJAR LOCAL
   
 
   getUsuario(){
